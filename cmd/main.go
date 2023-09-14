d46e7370602bb6393c958809cef1a15fb6a22e10
Cr4z1k <90409717+Cr4z1k@users.noreply.github.com>
2023-09-14 22:07:23 +0300

    Created endpoint for adding courses

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/KrizzMU/coolback-alkol/db"
 	"github.com/KrizzMU/coolback-alkol/handlers"
+	"github.com/KrizzMU/coolback-alkol/handlers/courses_handlers"
 	"github.com/KrizzMU/coolback-alkol/repository"
 
 	"github.com/gin-gonic/gin"
@@ -15,7 +16,7 @@ func main() {
 	defer repo.CloseConnection()
 
 	router.Handle("GET", "/", handlers.TestHandler(repo))
-	
+	router.Handle("POST", "/add/course", courses_handlers.AddCourseHandler(repo))
 
 	router.Run(":8080")
 }
