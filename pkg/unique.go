d4793df348ed630cf6074439cf814fbcfb138ac4
Cr4z1k <mr.kolya560@mail.ru>
2023-09-23 22:35:20 +0300

    New logic for unique folder/file. Also new func to get path to the folder/file by name

--- pkg/unique.go
+++ pkg/unique.go
@@ -1,32 +1,86 @@
 package pkg
 
 import (
+	"fmt"
+	"io"
+	"mime/multipart"
 	"os"
 	"path/filepath"
-
-	"github.com/google/uuid"
+	"regexp"
+	"strings"
 )
 
-func UniqueFolder(name string, folder string) (string, error) {
-	for {
-		uniqueID := uuid.New().String()
-		uniqueName := name + "_" + uniqueID
-		filePath := filepath.Join(folder, uniqueName)
-		_, err := os.Stat(filePath)
-		if os.IsNotExist(err) {
-			return filePath, nil
-		}
-	}
+func GetPath(name string, folder string) string {
+	validName := regexp.MustCompile(`[^\p{L}0-9\s-]`).ReplaceAllString(name, "")
+
+	words := strings.Fields(validName)
+	uniqueName := strings.Join(words, "-")
+
+	dirPath := filepath.Join(folder, uniqueName)
+
+	return dirPath
 }
 
-func UniqueFile(ext string, folder string) (string, error) {
-	for {
-		uniqueID := uuid.New().String()
-		uniqueName := "lesson_" + uniqueID + ext
-		filePath := filepath.Join(folder, uniqueName)
-		_, err := os.Stat(filePath)
-		if os.IsNotExist(err) {
-			return filePath, nil
+func CreateUniqueFolder(name string, folder string) (string, error) {
+	validName := regexp.MustCompile(`[^\p{L}0-9\s-]`).ReplaceAllString(name, "")
+
+	words := strings.Fields(validName)
+	uniqueName := strings.Join(words, "-")
+
+	dirPath := filepath.Join(folder, uniqueName)
+
+	splitFileName := strings.Split(dirPath, "\\")
+	dbFolderName := splitFileName[len(splitFileName)-1]
+
+	_, err := os.Stat(dirPath)
+	if os.IsNotExist(err) {
+		err := os.Mkdir(dirPath, os.ModePerm)
+		if err != nil {
+			return "", err
 		}
+		return dbFolderName, nil
 	}
+
+	return "", fmt.Errorf("directory already exists")
+}
+
+func CreateUniqueFile(file multipart.File, name string, folder string, requiredExt string) (string, error) {
+	ext := filepath.Ext(name)
+	if ext != requiredExt {
+		return "", fmt.Errorf("wrong file extension")
+	}
+
+	nameWithoutExt := name[:len(name)-len(ext)]
+
+	validName := regexp.MustCompile(`[^\p{L}0-9\s-]`).ReplaceAllString(nameWithoutExt, "")
+
+	words := strings.Fields(validName)
+	uniqueName := strings.Join(words, "-")
+
+	filePath := filepath.Join(folder, uniqueName+ext)
+
+	_, err := os.Stat(filePath)
+	if os.IsNotExist(err) {
+		out, err := os.Create(filePath)
+		if err != nil {
+			return "", err
+		}
+
+		_, err = io.Copy(out, file)
+		if err != nil {
+			return "", err
+		}
+
+		if err = out.Close(); err != nil {
+			return "", err
+		}
+
+		return uniqueName + ext, nil
+	}
+
+	if err = file.Close(); err != nil {
+		return "", err
+	}
+
+	return "", fmt.Errorf("file already exists")
 }
