7a3a40225d17e186609c604d289909921f10a7f6
Cr4z1k <mr.kolya560@mail.ru>
2023-09-24 21:03:25 +0300

    Add constraints to models

--- internal/core/models.go
+++ internal/core/models.go
@@ -6,7 +6,7 @@ type Course struct {
 	gorm.Model
 
 	Name        string `gorm:"not null; unique"`
-	Description string
+	Description string `gorm:"not null"`
 	NameFolder  string `gorm:"not null"`
 }
 
@@ -14,7 +14,7 @@ type Module struct {
 	gorm.Model
 
 	Name        string `gorm:"not null"`
-	Description string
+	Description string `gorm:"not null"`
 	NameFolder  string `gorm:"not null"`
 	CourseID    uint
 
@@ -25,7 +25,7 @@ type Lesson struct {
 	gorm.Model
 
 	Name        string `gorm:"not null"`
-	Description string
+	Description string `gorm:"not null"`
 	NameFile    string `gorm:"not null"`
 	ModuleID    uint
 
